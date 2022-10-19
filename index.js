b44e454bcdc3c3cbf8da315186e587b38f0bc430
Ontaria Woods <ontaria.woods@gmail.com>
2022-10-19 17:29:22 -0400

    commit tree ccbc40ce828adb9a9c43691d8110effab476482a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+  let message = [];
+  for (let i = 0; i < name.length; i++) {
+    message.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+  }
+  return message;
+}
+
+function countDown() {
+  let count = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let j = 0;
+  while (j < count.length) {
+    console.log(count[j++]);
+  }
+}
